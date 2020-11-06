4df67e96a621c560999661171dd71ad97482bb54
Abdullah Ahmed <abdullah.ahmed.a2000@gmail.com>
2020-11-06 18:22:56 +0200

    Fix a 404

--- routes/web.php
+++ routes/web.php
@@ -27,12 +27,12 @@ Route::prefix('admin')->middleware('admin')->group(function() {
     Route::post('admin', 'AdminController@update')->name('admin.admin.update');
 
     Route::get('competitions', 'AdminCompetitionsController@index')->name('admin.competitions.index');
-    Route::get('competitions/{competition}', 'AdminCompetitionsController@show')->name('admin.competitions.show');
     Route::get('competitions/create', 'AdminCompetitionsController@create')->name('admin.competitions.create');
     Route::get('competitions/{competition}/edit', 'AdminCompetitionsController@edit')->name('admin.competitions.edit');
     Route::patch('competitions/{competition}', 'AdminCompetitionsController@update')->name('admin.competitions.update');
     Route::post('competitions', 'AdminCompetitionsController@store')->name('admin.competitions.store');
     Route::delete('competitions/{competition}', 'AdminCompetitionsController@destroy')->name('admin.competitions.destroy');
+    Route::get('competitions/{competition}', 'AdminCompetitionsController@show')->name('admin.competitions.show');
 
     Route::get('participants', 'AdminParticipantController@index')->name('admin.participants.index');
     Route::get('participants/create', 'AdminParticipantController@create')->name('admin.participants.create');
